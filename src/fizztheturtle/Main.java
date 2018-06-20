36a3b7b0b8d5cd6c7d0f42edc204baa9cf9fc810
Faisal Kabeer <kabeerf12@outlook.com>
2018-06-20 19:28:15 +0100

    addition of sitelist from Metoffice and simple API key reader, is recreating requires unique key

--- src/fizztheturtle/Main.java
+++ src/fizztheturtle/Main.java
@@ -1,8 +1,45 @@
 package fizztheturtle;
 
+
+//"Contains public sector information licensed under the Open Government Licence"
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 	// write your code here
+        Main obj = new Main();
+        System.out.println(obj.getFile("fizztheturtle/API_Key.txt"));
     }
+
+    private String getFile(String fileName) {
+
+        StringBuilder result = new StringBuilder("");
+
+        //Get file from resources folder
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
+
+        try (Scanner scanner = new Scanner(file)) {
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                result.append(line).append("\n");
+            }
+
+            scanner.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+
+    }
+
 }
